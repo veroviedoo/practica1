e72065513d4e85e5cf3e59dfdc53266efb4d080c
deisy <oviedo.vero211@gmail.com>
2023-12-17 20:24:15 -0300

    first commit

---
+++ HolaMundo.java
@@ -0,0 +1,18 @@
+/**
+ * HolaMundo
+ * @author Veronica Oviedo
+ */
+public class HolaMundo {
+
+    //metodo constructor 
+ public HolaMundo () {
+}
+    //metodo principal
+
+   public static void main (String[] args) { 
+     System.out.println("Hola");
+
+    }
+
+  
+} 
