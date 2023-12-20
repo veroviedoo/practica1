8f752fb7fb5fd283ac38951d9652b89968529672
deisy <oviedo.vero211@gmail.com>
2023-12-20 00:03:52 -0300

    first commit

---
+++ practicaSWING/Formulario2.java
@@ -0,0 +1,21 @@
+package practicaSWING;
+
+/**
+ * @author Veronica Oviedo
+ * Formulario2
+ */
+import javax.swing.*;
+public class Formulario2 extends JFrame{
+    public Formulario2() {
+        setLayout(null);
+    }
+
+    public static void main(String[] ar) {
+        Formulario2 formulario1=new Formulario2();
+        formulario1.setBounds(10,20,400,300);
+        formulario1.setVisible(true);
+        formulario1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+    
---
+++ practicaSWING/Formulario4.java
@@ -0,0 +1,31 @@
+package practicaSWING;
+
+/**
+ * @author Veronica Oviedo
+ * Formulario4
+ */
+import javax.swing.*;
+import java.awt.event.*;
+public class Formulario4 extends JFrame implements ActionListener {
+    JButton boton1;
+    public Formulario4() {
+        setLayout(null);
+        boton1=new JButton("Finalizar");
+        boton1.setBounds(300,250,100,30);
+        add(boton1);
+        boton1.addActionListener(this);
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==boton1) {
+            System.exit(0);
+        }
+    }
+    
+    public static void main(String[] ar) {
+        Formulario3 formulario1=new Formulario3();
+        formulario1.setBounds(0,0,450,350);
+        formulario1.setVisible(true);
+        formulario1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
